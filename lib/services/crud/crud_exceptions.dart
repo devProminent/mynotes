ad3f25c769372bcd2eec81ab527a5bb67aa99271
Babalola Jeremiah <babalolajeremiah@Babalolas-MacBook-Air.local>
2022-07-17 15:44:46 +0100

    step-11

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,15 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentsDirectory implements Exception {}
+
+class DatabaseIsNotOpen implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNotDeleteNote implements Exception {}
+
+class CouldNotFindeNote implements Exception {}
+
+class CouldNotUpdateNote implements Exception {}

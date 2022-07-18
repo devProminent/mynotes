1b313a08615539d41737858dd34ebcaa1532d77c
Babalola Jeremiah <babalolajeremiah@Babalolas-MacBook-Air.local>
2022-07-18 14:01:50 +0100

    step-13

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -114,7 +114,10 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'error') throw WrongPasswordAuthException();
-    const user = AuthUser(isEmailVerified: false, email: '');
+    const user = AuthUser(
+      isEmailVerified: false,
+      email: 'jerry123.bj@gmail.com',
+    );
     _user = user;
     return Future.value(user);
   }
@@ -132,7 +135,10 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true, email: '');
+    const newUser = AuthUser(
+      isEmailVerified: true,
+      email: 'jerry123.bj@gmail.com',
+    );
     _user = newUser;
   }
 }
